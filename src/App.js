3ac725c0cfd0e625b9fbdfdfd370c0a526700ea6
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-12-27 17:47:40 -0600

    fix: solving image header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import MainHeader from "./components/MainHeader";
+import MainImage from "./components/MainImage";
 
 const App = () => {
   return(
     <div>
       <MainHeader />
+      <MainImage />
     </div>
   )
 }
---
+++ src/components/MainImage.jsx
@@ -0,0 +1,19 @@
+
+import Box from '@mui/material/Box';
+
+const MainImage = () => {
+    return(
+        <Box
+        component="img"
+        sx={{
+          height: '100%',
+          width: '100%',
+          maxHeight: { xs: 400, md: 700 },
+        }}
+        alt="The house from the offer."
+        src="https://picsum.photos/1920/1080"
+      />
+    )
+}
+
+export default MainImage;
