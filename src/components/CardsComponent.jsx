8c1a8085a192f9bf6b3f320fdf1a38e140435bb8
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-12-29 18:51:49 -0600

    refactor: adding files

--- src/components/CardsComponent.jsx
+++ src/components/CardsComponent.jsx
@@ -50,7 +50,7 @@ const CardsComponent = () => {
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Módulos</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Reportes</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
@@ -72,12 +72,60 @@ const CardsComponent = () => {
                     </div>
                 </div>
 
-                {/* <!-- second submenu--> */}
+                {/* <!-- Second Modal first submenu--> */}
+                <div class="modal fade" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel3" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel3">Manuales de procedimientos y protocolos de actuación</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li className="mb-2"><a href="https://auth.signalapp.net/auth/realms/Everbridge/protocol/openid-connect/auth?client_id=Signal&redirect_uri=https%3A%2F%2Feverbridge.signalapp.net%2Fdashboard&state=3bbb26bf-9bf2-4ca8-8eee-c9e22b13695b&response_mode=fragment&response_type=code&scope=openid&nonce=c7470756-93ee-4ee8-9cfe-50936a3b05bd" target="_blank">GRS - Gestor de redes sociales</a></li>
+                                <li className="mb-2"><a href="#" target="_blank">GPTC - Gestor de protocolos tareas y contactos</a></li>
+                                <li className="mb-2"><a href="#" target="_blank">GAD - Gestor de alertas en dispositivos</a></li>
+                                <li className="mb-2"><a href="#" target="_blank">VEX - Visor de eventos externos</a></li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <!-- first submenu PDF 1--> */}
                 <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
                     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="exampleModalLabel1">Documento Maestro</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                            <object data="./Manual_de_operaciones_C2SIC.pdf" type="application/pdf" width="770px" height="550px">
+                                <embed src="./Manual_de_operaciones_C2SIC.pdf" type="application/pdf"/>       
+                            </object>
+                                
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <!-- second submenu PDF 1--> */}
+                <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel2">Documento Maestro</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
@@ -108,7 +156,7 @@ const CardsComponent = () => {
                 <a href="#"  class={styles.anchorStyles} style={styles.mainButton}>
                     Cuadro de Mando Integral y Solución de Gestión Integral de Explotación de Información
                 </a>
-                <a href="#"  class={styles.anchorStyles} style={styles.mainButton}>
+                <a href="#"  class={styles.anchorStyles} style={styles.mainButton} data-toggle="modal" data-target="#exampleModal2">
                     Protocolos de Actuación, Manual de Procesos y Procedimientos Técnicos y Operativos
                 </a>
             </div>
@@ -118,7 +166,7 @@ const CardsComponent = () => {
                 <hr className="my-5 bg-white"/>
                 <div className="d-flex justify-content-between row ">
 
-                    <a href="#" className={styles.secondaryAnchorStyles} style={styles.secondaryButton}>
+                    <a href="#" className={styles.secondaryAnchorStyles} style={styles.secondaryButton} data-toggle="modal" data-target="#exampleModal3">
                     Plataforma Tecnológica
                     </a>
                     
@@ -131,7 +179,9 @@ const CardsComponent = () => {
                         Automatización de reportes manuales
                     </a>
                     <a href="#"  className={styles.secondaryAnchorStyles} style={styles.secondaryButton}>
-                        Conexión a Bases de Datos complementarias necesarias para mejorar los procesos
+                        {/* Conexión a Bases de Datos complementarias necesarias para mejorar los procesos */}
+                        Base de datos o tablas, empleados <br />
+                        Vehículos
                     </a>
                 </div>    
             </div>        
